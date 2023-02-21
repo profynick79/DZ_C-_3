f57d1a6e5dd7e0ff5f3e4a9897f364ac23a2e7dc
profynick_lep <nnicklep@gmail.com>
2023-02-21 19:57:30 +0300

    Добавлено решение задачи 23

--- Program.cs
+++ Program.cs
@@ -135,19 +135,31 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Введите координату точки А по оси Х: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату точки А по оси Y: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату точки А по оси Z: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату точки В по оси Х: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату точки В по оси Y: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату точки B по оси Z: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
-var result = Math.Sqrt(Math.Pow(x2 - x1, 2)+ Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine($"Результат: {result, 2}");
+// Console.Write("Введите координату точки А по оси Х: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату точки А по оси Y: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату точки А по оси Z: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату точки В по оси Х: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату точки В по оси Y: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату точки B по оси Z: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// var result = Math.Sqrt(Math.Pow(x2 - x1, 2)+ Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// Console.WriteLine($"Результат: {result, 2}");
 
 
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// for(начало; условие; увеличение переменной - счетчика)
+for(int i = 1; i <= N; i++)
+{
+    Console.Write(Math.Pow(i, 3) + "\t");
+}
