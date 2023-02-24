5937ae89c640df7b458f3f5fe9ef9a3edb421aa6
profynick_lep <nnicklep@gmail.com>
2023-02-24 19:39:46 +0300

    Добавлено решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -1,45 +1,8 @@
-﻿// int number = 123456;
-// Console.WriteLine(number / 100 % 10);
-
-//Поиск цифры по индексу через строку (строка всегда есть массив)
-//string testNumber = Console.ReadLine();
-//Console.WriteLine(testNumber[0]);
-//Console.WriteLine(testNumber[1]);
-//Console.WriteLine(testNumber[2]);
-
-
-// var test = 10;
-// var result = 100 * 0.5 / 2;
-// var test1 = "Hello World";
-// Console.WriteLine();
-
-//ЗАДАЧА 1
-// Вводится строчка. Необходимо проверить, являются ли 
-// все введенные символы числами
-// "1234" - подходит, в строчке число 1234, подходит
-// "1234Привет" - не подходит
-
-
-
-// var stringNumber = "1234Hello";
-// int number; // если строчка состоит из чисел, то сюда попадает наше число
-// bool isNumber = int.TryParse(stringNumber, out number); // True - строчка состоит из чисел, False - есть символы, буквы и т.п.
-// if (isNumber) // isNumber == true
-// {
-//     Console.WriteLine("Перевод произошел успешно,в стрчоке находились ТОЛЬКО цифры");
-//     Console.WriteLine(number);
-// }
-// else
-// {
-// Console.WriteLine("Перевод провалился: в строчке есть не только цифры");
-//     Console.WriteLine(number);
-// }
-
-
-// Задача 21
+﻿// Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
 
 // Console.Write("Введите координату точки А по оси Х: ");
 // int x1 = Convert.ToInt32(Console.ReadLine());
@@ -70,9 +33,26 @@
 //     Console.Write(Math.Pow(i, 3) + "\t");
 // }
 
-Задача 19
-Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-14212 -> нет
-12821 -> да
-23432 -> да
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+void CheckingNumber(string number)
+{
+if (number[0]==number[4] || number[1]==number[3])
+    {
+    Console.WriteLine($"Ваше число: {number} является палиндромом.");
+    }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите правильное число");
 
