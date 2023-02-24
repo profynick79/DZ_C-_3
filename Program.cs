85291219152163f20011b9c4165972eb06f8a0bc
profynick_lep <nnicklep@gmail.com>
2023-02-24 22:03:31 +0300

    исправлено решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -41,18 +41,17 @@
 
 Console.Write("Введите пятизначное число: ");
 string number = Console.ReadLine();
-void CheckingNumber(string number)
+void PolNumber(string number)
 {
 if (number[0]==number[4] || number[1]==number[3])
     {
     Console.WriteLine($"Ваше число: {number} является палиндромом.");
     }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+  else Console.WriteLine($"Ваше число: {number} - НЕ является палиндромом.");
 }
-
-if (number!.Length == 5)
+if (number.Length == 5)
 {
-  CheckingNumber(number);
+  PolNumber(number);
 }
 else Console.WriteLine($"Введите правильное число");
 
